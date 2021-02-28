5b9076b59eaa9ed3f202c3250b0701f49753405c
Carl Shen <carl.shen1@uwaterloo.ca>
2021-02-28 17:35:53 -0500

    setup tokenization and vocab

---
+++ model/model.py
@@ -0,0 +1,34 @@
+import numpy
+import sys
+import nltk
+# nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+
+def tokenize(text):
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(text)
+    print(tokens)
+    
+    filtered = filter(lambda token: token not in stopwords.words('english'), tokens)
+    # return list(filtered)
+    return " ".join(filtered)
+
+text = open("../smallData.txt").read().lower()
+# text = open("../storyData.txt").read().lower()
+
+# print(len(text))
+# a=tokenize(text)
+# print(a)
+lenText = len(text)
+
+chars = sorted(list(set(text)))
+chatToIndices = dict((c, i) for i, c in enumerate(chars))
+indicesToChar = dict((i, c) for i, c in enumerate(chars))
+numChars = len(chars)
+# print(chars)
+print('Unique Chars:', numChars)
+# print(char_indices['h'])
+# print(indices_char[3])
+
+print("Text length (chars):",lenText)
