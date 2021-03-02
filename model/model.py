895a01da79d2b959abeadaab8560b9e7fdcdd01f
Carl Shen <carl.shen1@uwaterloo.ca>
2021-03-01 23:57:45 -0500

    one hot encoding

--- model/model.py
+++ model/model.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import sys
 import nltk
 # nltk.download('stopwords')
@@ -15,7 +15,7 @@ def tokenize(text):
     return " ".join(filtered)
 def main(): 
     # text = open("../smallData.txt").read().lower()
-    text = open("../storyData.txt").read().lower()
+    text = open("../smallData.txt").read().lower()
 
     # print(len(text))
     # a=tokenize(text)
@@ -23,7 +23,7 @@ def main():
     lenText = len(text)
 
     chars = sorted(list(set(text)))
-    chatToIndices = dict((c, i) for i, c in enumerate(chars))
+    charToIndices = dict((c, i) for i, c in enumerate(chars))
     indicesToChar = dict((i, c) for i, c in enumerate(chars))
     numChars = len(chars)
     # print(chars)
@@ -43,9 +43,19 @@ def main():
         # print(text[i:i+seqLen])
         # print(text[i+seqLen])
         # print("------------")
-    print(dataPairs[3][1])
-    print(len(inputSeq))
+    # print(dataPairs[3][1])
+    # print(len(inputSeq))
 
+    # one hot encoding
+    X = np.zeros((len(inputSeq), seqLen, numChars), dtype=np.bool)
+    y = np.zeros((len(inputSeq), numChars), dtype=np.bool)
   
+    for i, seq in enumerate(inputSeq):
+        for t, char in enumerate(seq):
+            X[i, t, charToIndices[char]] = True
+        y[i, charToIndices[outputChar[i]]] = True
+    
+    print(X.shape)
+    print(y.shape)
 if __name__=="__main__": 
     main() 
