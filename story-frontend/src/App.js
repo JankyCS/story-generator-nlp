a7f1c8f3ad5a866781d829c185fac0386da41e86
Carl Shen <carl.shen1@uwaterloo.ca>
2021-03-17 19:28:33 -0400

    frontend init

---
+++ story-frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
