86d7365f14d47f96772297306bace43226063e5b
Carl Shen <carl.shen1@uwaterloo.ca>
2021-03-17 23:06:46 -0400

    frontend skeleton

--- story-frontend/src/components/mainPage.js
+++ story-frontend/src/components/mainPage.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function mainPage(props) {
     return(
-        <div>
-        pog
+        <div className="container-fluid poppin">
+        <div className="row align-items-center" style={{textAlign:"center",verticalAlign: "middle"}}>
+            <div className="col-lg-12" style={{height:"0vh",verticalAlign: "middle",textAlign:"Center",padding:"0vh 5vw 0px 5vw"}}>
+              <h1 style={{fontSize:45,fontWeight:700}}>Story Helper thing.</h1>
+              <p>
+                <b>STORY HELPER</b> does stuff.
+              </p>
+                <br/>
+             
+              <br/>
+            </div>
         </div>
+      </div>
     )
 }
 
