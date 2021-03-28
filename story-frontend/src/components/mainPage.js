536d7a830ccc5514d0b017daec6040cfe3d7fb11
Carl Shen <carl.shen1@uwaterloo.ca>
2021-03-28 18:40:16 -0400

    cleanup

--- story-frontend/src/components/mainPage.js
+++ story-frontend/src/components/mainPage.js
@@ -28,30 +28,10 @@ function MainPage(props) {
                 }
                 setStoryText(old => old+r.addedText)   
             }
-            // var dt = new Date();
-            // while ((new Date()) - dt <= 500) {}
+            var dt = new Date();
+            while ((new Date()) - dt <= 200) {}
             getWords()
         }
-        // else{
-        //     const spellingGrammar = async () => {
-        //         let body = {
-        //             inputText:storyText
-        //         }
-
-        //         let requestOptions = {
-        //             method: 'POST',
-        //             headers: { 'Content-Type': 'application/json' },
-        //             body: JSON.stringify(body)
-        //         }
-
-        //         console.log(requestOptions)
-        //         var i = await fetch('http://localhost:5000/grammar', requestOptions)
-        //         var r = await i.json()
-        //         setStoryText(old => r.newText)   
-        //     }
-
-        //     spellingGrammar()
-        // }
     }, [storyText,paused]);
 
     const spellingGrammar = async () => {
@@ -82,11 +62,11 @@ function MainPage(props) {
         <div className="container-fluid poppin">
         <div className="row align-items-center" style={{textAlign:"center",verticalAlign: "middle"}}>
             <div className="col-lg-12" style={{height:"0vh",verticalAlign: "middle",textAlign:"Center",padding:"0vh 5vw 0px 5vw"}}>
-              <h1 style={{fontSize:45,fontWeight:700}}>Story Buddy.</h1>
+              <h1 style={{fontSize:45,fontWeight:700}}>Writing Buddy.</h1>
               <p
-                style = {{width:500, margin:"auto"}}
+                style = {{width:"60vw", margin:"auto"}}
               >
-                <b>STORY BUDDY</b> is a proof of concept/prototype, AI writing assistant, to help provide direction and overcome writing blocks.
+                <b>WRITING BUDDY</b> is a proof of concept/prototype, AI writing assistant, to help provide direction and overcome writing blocks.
                 The dataset is comprised of 153 short stories, scraped from
                 <a href="http://www.classicshorts.com/bib.html"> Classic Shorts</a>. It contains 3422389 characters and 611454 words,
                 which ultimately amounts to 855649 training examples for the neural network. 
@@ -95,9 +75,6 @@ function MainPage(props) {
                 <button type="button" className="btn btn-success" onClick={toggleGeneration}>
                     {paused ? "Generate Text" : "Pause Generation"}
                 </button>
-                {/* <button type="button" className="btn btn-success" onClick={spellingGrammar} disabled={!paused ? "disabled" : ""}>
-                    Grammar Check
-                </button> */}
                 <br />
                 <br />
                 <div className="form-group" style={{height:"40vh", width:"60vw", margin: "auto"}}>
