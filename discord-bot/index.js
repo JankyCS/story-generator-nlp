dbdf79270da47e1758d25866799399cd5a413832
Carl Shen <carl.shen1@uwaterloo.ca>
2021-04-04 03:24:53 -0400

    command flow

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -13,7 +13,7 @@ client.on("guildCreate", guild => {
     console.log("Joined a new guild: " + guild.name);
     var server = message.guild;
     var name = message.author.username;
-    if(guild.channels.find(x => x.name == BOT_CHANNEL) == null){
+    if(guild.channels.cache.find(x => x.name == BOT_CHANNEL) == null){
         guild.createChannel(BOT_CHANNEL)
     }
 })
@@ -23,19 +23,18 @@ client.on("ready", () => {
 })
 
 client.on("message", msg => {
-    
+    if (msg.author.bot) return;
     // console.log(msg.content)
     // console.log(msg)
     if(msg.content === PREFIX+"createChannel"){
         const guild = msg.guild
-
         if(guild.channels.cache.find(channel => channel.name == BOT_CHANNEL) == null){
-            guild.channels.create(BOT_CHANNEL, { type: 'text',  reason: 'New channel added for fun!' }).then(c =>{
+            guild.channels.create(BOT_CHANNEL, { type: 'text',  reason: 'Story Writing Channel' }).then(c =>{
+                c.send("INFO ON HOW THE BOT WORKS, ETC.....")
                 msg.reply("Channel Created")
-                console.log(guild.channels.cache)
-                // msg.reply(guild.channels.cache.toString())
             }).catch(e =>
                 {
+                    msg.reply("Error creating channel.")
                     console.log(e)
                 }
             )
@@ -44,9 +43,37 @@ client.on("message", msg => {
             msg.reply("Channel Already Exists")
         }
     }
-
-    if(msg.channel.name == BOT_CHANNEL){
-        console.log("poggers")
+    else if(msg.channel.name == BOT_CHANNEL){
+        if(!msg.content.startsWith(PREFIX)){
+            msg.delete({ timeout: 100 });
+            msg.reply('Only bot commands allowed!')
+            .then(m => {
+                m.delete({ timeout: 5000 })
+            })
+        }
+        else{
+            const words = msg.content.split(/\s+/)
+            const command = words[0].substring(PREFIX.length,).toLowerCase()
+            msg.reply(command)
+            switch(command){
+                case "write":
+                    msg.reply('write')
+                    .then(m => {
+                        m.delete({ timeout: 5000 })
+                    })
+                    break;
+                case "read":
+                    // code block
+                    break;
+                default:
+                    msg.reply('Invalid Command')
+                    .then(m => {
+                        m.delete({ timeout: 5000 })
+                    })
+                
+            }
+        }
     }
+
     // msg.channel.send(BOT_CHANNEL)
 })
